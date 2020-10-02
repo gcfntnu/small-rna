a6e27d7f472037d2a9aedc090b3109c04466c391
flatberg <arnar.flatberg@ntnu.no>
2020-10-02 13:03:04 +0200

    bfq rules updates

--- rules/quant/scripts/create_anndata.py
+++ rules/quant/scripts/create_anndata.py
@@ -18,16 +18,56 @@ def argparser():
                         help='Optional sample sheet. Will subset expr table if needed')
     parser.add_argument('--feature-info', dest='features',
                         help='Optional sample sheet. Will subset expr table if needed')
+    parser.add_argument('--add-vsn', dest='vsn', action='store_true',
+                        help='Add layer with variance transformed data')
     parser.add_argument('-o ', '--output', default='adata.h5ad',
                         help='Output filename. Will default to pca_mqc.png, Optional [*.h5ad]')
 
     args = parser.parse_args()
     return args
 
+
+def vsn(adata, method='auto'):
+    import anndata2ri
+    from anndata2ri.rpy2_ext import importr
+    from rpy2.robjects import r
+    from rpy2.robjects import globalenv
+    from rpy2.robjects.conversion import localconverter
+    import rpy2.robjects.numpy2ri
+    rpy2.robjects.numpy2ri.activate()
+    importr('DESeq2')
+    importr('scran')
+
+    globalenv["adata"] = adata.X
+    #with localconverter(anndata2ri.converter):
+    #    globalenv["adata"] = adata
+    #r('de <- convertTo(adata, type="DESeq2")')
+    #r('de <- estimateSizeFactors(de)')
+    #r('de <- estimateDispersions(de)')
+
+    if method == 'auto':
+        if adata.shape[0] > 50:
+            method = 'deseq2_vst'
+        else:
+            method = 'deseq2_rlog'
+    
+    if method == 'deseq2_vst':
+        r('vsd <- varianceStabilizingTransformation(adata+1, fitType="mean")')
+    elif method == 'deseq2_rlog':
+        r('vsd <- rlogTransformation(adata+1, fitType="mean")')
+    else:
+        raise ValueError
+    X = np.array(r.vsd)
+    adata.layers['vsd'] = pd.DataFrame(X, index=adata.obs_names, columns=adata.var_names)
+    adata.uns['varians_transform'] = method
+    return adata
+
+        
 if __name__ == "__main__":
     args = argparser()
     
     X = pd.read_csv(args.input, sep="\t", index_col=0).T
+    X = X.astype(np.uint64)
     X.columns = X.columns.astype(str)
 
     S = F = None
@@ -50,5 +90,8 @@ if __name__ == "__main__":
 
     adata = ad.AnnData(X=X, obs=S, var=F)
 
+    if args.vsn:
+        adata = vsn(adata)
+    
     adata.write(filename=args.output)
     
